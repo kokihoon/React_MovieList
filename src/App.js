6abbe32e625b4166742c86a4112101719a9c240d
kokihoon <rhrlgns777@gmail.com>
2019-01-26 23:11:40 +0900

    Promises

--- src/App.js
+++ src/App.js
@@ -11,38 +11,12 @@ class App extends Component {
 
   state = {
   }
-  
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            // id:1,
-            title :"샹크스",
-            poster : "https://t1.daumcdn.net/cfile/tistory/214ACE49536F9C1118"
-          },
-          {
-            // id:2,
-            title :"루피",
-            poster : "https://scontent-lga3-1.cdninstagram.com/vp/84ce4014bc8a43db90738624e61c848c/5CD55733/t51.2885-15/e35/31255802_162447204423964_8785540863087345664_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&ig_cache_key=MTc3Njc2OTA1ODIzNjQ4MzI4Mg%3D%3D.2"
-          },
-          {
-            // id:3,
-            title :"검은수염",
-            poster : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6GZJqsUGCh1w52A_SPcM0dEQE70wEGmgagNGfyIxlNFTc4jc9oA"
-          },
-          {
-            // id:4,
-            title :"카이도우",
-            poster : "https://t1.daumcdn.net/cfile/tistory/2678F33E58385A772E"
-          },
-          {
-            title: "벤 베크만",
-            poster: "http://static.inven.co.kr/image_2011/site_image/game/minidata/84/character_0817_c1.jpg"
-          }
-        ]
-      })
-    }, 1000)
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=like_count')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
   }
 
   _renderMovies = () => {
