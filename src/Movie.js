07e3cfb850d967b3bbc9544ca1bdf8252e6c7cf5
kokihoon <rhrlgns777@gmail.com>
2019-01-26 13:27:51 +0900

    Dataflow with Props

--- src/Movie.js
+++ src/Movie.js
@@ -3,10 +3,11 @@ import './Movie.css';
 
 class Movie extends Component{
     render(){
+        //console.log(this.props);
         return (
             <div>
-                <MoviePoster/>
-                <h1>Hello this is Movie.</h1>
+                <MoviePoster poster={this.props.poster}/>
+                <h1>{this.props.title}</h1>
             </div>
         );
     }
@@ -14,8 +15,9 @@ class Movie extends Component{
 
 class MoviePoster extends Component {
     render(){
+        console.log(this.props.poster)
         return (
-            <img src="https://scontent-lga3-1.cdninstagram.com/vp/84ce4014bc8a43db90738624e61c848c/5CD55733/t51.2885-15/e35/31255802_162447204423964_8785540863087345664_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&ig_cache_key=MTc3Njc2OTA1ODIzNjQ4MzI4Mg%3D%3D.2"/>
+            <img src={this.props.poster}/>
         )
     }
 }
